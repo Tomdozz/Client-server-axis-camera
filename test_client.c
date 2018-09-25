f997c8133aa5917452ab171aa94a9ba388fa4ce4
Tomdozz <tom_1995@msn.com>
2018-09-25 12:38:51 +0200

    installed server on camera

--- test_client.c
+++ test_client.c
@@ -23,7 +23,7 @@ int main(){
 	if(connection_status == -1){
 		printf("There was an error making the connection");
 	}
-	char *hello_message = "helo frosdaasdasdaasm clien";
+	char *hello_message = "elo rosdaasdasdaasm clien";
 	send(network_socket, hello_message, strlen(hello_message),0);
 	char server_response[256];
 	//recive data
